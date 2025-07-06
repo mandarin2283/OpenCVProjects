91a2e689d41645a2693fae4d56ffc4c595a24182
mandarin2283 <manda4in2283@gmail.com>
2025-07-06 14:43:38 +0300

    Finish image classifier

--- Features/ImageClassifier.py
+++ Features/ImageClassifier.py
@@ -25,19 +25,25 @@ def find_descriptor(images):
     return des_list
 
 
-def find_id(img,des_list):
+def find_id(img,des_list,thres=10):
     kp2,des2 = orb.detectAndCompute(img,None)
     bf = cv2.BFMatcher()
     match_list = []
     final_value = -1
-    for des in des_list:
-        matches = bf.knnMatch(des, des2, k=2)
-        good = []
-        for m, n in matches:
-            if m.distance < 0.75 * n.distance:
-                good.append([m])
-        match_list.append(len(good))
-    print(match_list)
+    try:
+        for des in des_list:
+            matches = bf.knnMatch(des, des2, k=2)
+            good = []
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    good.append([m])
+            match_list.append(len(good))
+    except:
+        pass
+    if len(match_list)!= 0:
+        if max(match_list) > thres:
+            final_value = match_list.index(max(match_list))
+    return final_value
 
 
 descriptors = find_descriptor(images)
@@ -50,7 +56,10 @@ while True:
     image_original = image.copy()
     image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-    find_id(image,descriptors)
+    id = find_id(image,descriptors)
+    if id != -1:
+        cv2.putText(image_original,class_names[id],(50,50)
+                    ,cv2.FONT_ITALIC,1,(0,0,255),1)
 
     cv2.imshow('result',image_original)
 

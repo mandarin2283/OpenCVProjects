67d85158b6c29ba84f2cd515b24c097c8ec6cd64
mandarin2283 <manda4in2283@gmail.com>
2025-07-10 03:09:14 +0300

    RESET

--- Hands/GuiSimulate.py
+++ Hands/GuiSimulate.py
@@ -16,9 +16,14 @@ while True:
     lm_list = detector.find_pos(img)
 
     if len(lm_list)!=0:
-        length = detector.find_distance(lm_list[4][1:],lm_list[8][1:])
+        length_to_space = detector.find_distance(lm_list[4][1:],lm_list[8][1:])
+        length_to_mouse = detector.find_distance(lm_list[8][1:],lm_list[12][1:])
+        bent_list = detector.is_bent(lm_list)
 
-        if length<50:
+        if bent_list[1]==0:
+            gui.move(lm_list[8][1],lm_list[8][2])
+
+        if length_to_space<50:
             gui.press('space')
 
     cv2.imshow('result', img)

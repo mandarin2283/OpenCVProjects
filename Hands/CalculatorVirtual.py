d7929dba7fee0e7b7e3cf93455e4da45033424f0
mandarin2283 <manda4in2283@gmail.com>
2025-07-19 23:56:49 +0300

    Add main construction

--- Hands/CalculatorVirtual.py
+++ Hands/CalculatorVirtual.py
@@ -38,68 +38,75 @@ class Button:
             return False
 
 
-cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
-detector = htm.HandDetector()
-
-button_values = [['7','8','9','*'],
-                 ['4','5','6','-'],
-                 ['1','2','3','+'],
-                 ['0','/','.','='],
-                 ]
-button_list = []
-for x in range(4):
-    for y in range(4):
-        xpos = x*100 + 600
-        ypos = y*100 + 100
-        button_list.append(Button((xpos, ypos), 100, 100,
-                                  button_values[y][x]))
-
-my_equation = ''
-counter = 0
 
 
-while True:
-    suc, img = cap.read()
-    img = cv2.flip(img,1)
 
-    img = detector.find_hands(img)
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    detector = htm.HandDetector()
 
-    cv2.rectangle(img,(600,50),(1000,170),
-                  (255,255,255),-1)
-    cv2.rectangle(img,(600,50),(1000,170),
-                  (50,50,50),3)
-    for button in button_list:
-        button.draw(img)
+    button_values = [['7', '8', '9', '*'],
+                     ['4', '5', '6', '-'],
+                     ['1', '2', '3', '+'],
+                     ['0', '/', '.', '='],
+                     ]
+    button_list = []
+    for x in range(4):
+        for y in range(4):
+            xpos = x * 100 + 600
+            ypos = y * 100 + 100
+            button_list.append(Button((xpos, ypos), 100, 100,
+                                      button_values[y][x]))
 
-    lm_list = detector.find_pos(img)
+    my_equation = ''
+    counter = 0
 
-    if len(lm_list)!=0:
-        length = detector.find_distance(lm_list[8][1:],lm_list[12][1:])
-        x,y = lm_list[8][1:]
-        if length<50:
-            for i,button in enumerate(button_list):
-                if button.click(x,y,img) and counter==0:
-                    my_value = button.value
-                    if my_value=='=':
-                        my_equation = str(eval(my_equation))
-                    else:
-                        my_equation += my_value
-                    counter = 1
-    if counter != 0:
-        counter += 1
-        if counter>10:
-            counter = 0
+    while True:
+        suc, img = cap.read()
+        img = cv2.flip(img, 1)
 
-    cv2.putText(img, my_equation, (610,90),
-                cv2.FONT_HERSHEY_PLAIN,
-                3, (50, 50, 50), 3)
+        img = detector.find_hands(img)
 
-    cv2.imshow('result', img)
+        cv2.rectangle(img, (600, 50), (1000, 170),
+                      (255, 255, 255), -1)
+        cv2.rectangle(img, (600, 50), (1000, 170),
+                      (50, 50, 50), 3)
+        for button in button_list:
+            button.draw(img)
 
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-    elif key == ord('w'):
-        my_equation = ''
+        lm_list = detector.find_pos(img)
+
+        if len(lm_list) != 0:
+            length = detector.find_distance(lm_list[8][1:], lm_list[12][1:])
+            x, y = lm_list[8][1:]
+            if length < 45:
+                for i, button in enumerate(button_list):
+                    if button.click(x, y, img) and counter == 0:
+                        my_value = button.value
+                        if my_value == '=':
+                            my_equation = str(eval(my_equation))
+                        else:
+                            my_equation += my_value
+                        counter = 1
+        if counter != 0:
+            counter += 1
+            if counter > 10:
+                counter = 0
+
+        cv2.putText(img, my_equation, (610, 90),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    3, (50, 50, 50), 3)
+
+        cv2.imshow('result', img)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('w'):
+            my_equation = ''
+
+
+if __name__ == '__main__':
+    main()
